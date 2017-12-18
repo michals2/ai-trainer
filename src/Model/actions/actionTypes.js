629de8c99d7d3e9c082db2b0b61e3d2f7e58218d
Luke <luke.michals@gmail.com>
2017-12-17 16:46:26 -0800

    add actions for label tool

--- src/Model/actions/actionTypes.js
+++ src/Model/actions/actionTypes.js
@@ -1,2 +1,5 @@
 export const INCREMENT_STEP = "INCREMENT_STEP";
 export const DECREMENT_STEP = "DECREMENT_STEP";
+
+export const ADD_LABEL = "ADD_LABEL";
+export const DELETE_LABEL = "DELETE_LABEL";
--- src/Model/actions/actions.js
+++ src/Model/actions/actions.js
@@ -6,3 +6,10 @@ export function incrementStep() {
 export function decrementStep() {
   return { type: types.DECREMENT_STEP };
 }
+
+export function addLabel(shape, dimensions, file) {
+  return { type: types.ADD_LABEL, shape, dimensions, file };
+}
+export function deleteLabel(file, labelID) {
+  return { type: types.ADD_LABEL, file, labelID };
+}
