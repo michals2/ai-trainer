98b5b0930bf73effd85c8c78ad7c57055e2c3a51
Luke <luke.michals@gmail.com>
2017-12-19 18:33:57 -0800

    Add generic file uploader

--- src/Components/FileUploader.jsx
+++ src/Components/FileUploader.jsx
@@ -1,8 +1,33 @@
 // React imports
 import React from "react";
 
+import { Upload, Icon, message } from "antd";
+const Dragger = Upload.Dragger;
+
+const props = {
+  name: "file",
+  multiple: true,
+  action: "./",
+  onChange(info) {
+    console.log({ info });
+  }
+};
+
 const FileUploader = () => {
-  return <div>FileUploader placeholder</div>;
+  return (
+    <Dragger {...props}>
+      <p className="ant-upload-drag-icon">
+        <Icon type="inbox" />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibit from uploading
+        company data or other band files
+      </p>
+    </Dragger>
+  );
 };
 
 export default FileUploader;
