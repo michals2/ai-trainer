7e715adc851dc85a279fc0c25ce5d6a1d652c8d9
Luke <luke.michals@gmail.com>
2017-12-19 22:21:38 -0800

    Add custom image uploader

--- src/Components/FileUploader.jsx
+++ src/Components/FileUploader.jsx
@@ -1,33 +1,70 @@
 // React imports
-import React from "react";
+import React, { Component } from "react";
 
-import { Upload, Icon, message } from "antd";
-const Dragger = Upload.Dragger;
-
-const props = {
-  name: "file",
-  multiple: true,
-  action: "./",
-  onChange(info) {
-    console.log({ info });
+class FileUploader extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    };
   }
-};
 
-const FileUploader = () => {
-  return (
-    <Dragger {...props}>
-      <p className="ant-upload-drag-icon">
-        <Icon type="inbox" />
-      </p>
-      <p className="ant-upload-text">
-        Click or drag file to this area to upload
-      </p>
-      <p className="ant-upload-hint">
-        Support for a single or bulk upload. Strictly prohibit from uploading
-        company data or other band files
-      </p>
-    </Dragger>
-  );
-};
+  handleChange = e => {
+    const files = [...this.refs.input.files];
+    console.log(this.refs.input, { files });
+
+    var reader = new FileReader();
+    reader.onload = event => {
+      const url = event.target.result;
+      console.log({ url });
+      this.setState({ images: [...this.state.images, url] });
+    };
+
+    reader.readAsDataURL(files[0]);
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          multiple
+          id="the-file-input"
+          type="file"
+          ref="input"
+          onChange={this.handleChange}
+        />
+        {this.state.images.map((image, i) =>
+          <img width={100} key={i} src={image} />
+        )}
+      </div>
+    );
+  }
+}
 
 export default FileUploader;
+
+/*
+// render the image in our view
+function renderImage(file) {
+  
+    // generate a new FileReader object
+    var reader = new FileReader();
+  
+    // inject an image with the src url
+    reader.onload = function(event) {
+      the_url = event.target.result
+      $('#some_container_div').html("<img src='" + the_url + "' />")
+    }
+  
+    // when the file is read it triggers the onload event above.
+    reader.readAsDataURL(file);
+  }
+  
+  // handle input changes
+  $("#the-file-input").change(function() {
+      console.log(this.files)
+  
+      // grab the first image in the FileList object and pass it to the function
+      renderImage(this.files[0])
+  });
+  */
