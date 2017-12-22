8d085091652a9c06630e9a79959dd4b3d3b715fd
Luke <luke.michals@gmail.com>
2017-12-22 10:26:16 -0800

    Add form

--- src/Components/LabelSpecifier.jsx
+++ src/Components/LabelSpecifier.jsx
@@ -1,8 +1,41 @@
 // React imports
 import React from "react";
+import { Form, Input, Row, Col } from "antd";
+const FormItem = Form.Item;
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 }
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 }
+  }
+};
+
+const handleSubmit = e => {
+  e.preventDefault();
+  console.log({ e });
+};
 
 const LabelSpecifier = () => {
-  return <div>LabelSpecifier placeholder</div>;
+  return (
+    <div>
+      <Row type="flex" justify="space-between" span={24}>
+        <Col>
+          <Form onSubmit={handleSubmit}>
+            <FormItem {...formItemLayout} label="E-mail" required>
+              <Input />
+            </FormItem>
+            <FormItem {...formItemLayout} label="Password" required>
+              <Input type="password" />
+            </FormItem>
+          </Form>
+        </Col>
+        <Col>col-10</Col>
+      </Row>
+    </div>
+  );
 };
 
 export default LabelSpecifier;
