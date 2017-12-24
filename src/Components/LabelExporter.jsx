aeb960b52215c2253d35dc23d59dcb09f3c98d57
Luke <luke.michals@gmail.com>
2017-12-23 19:54:45 -0800

    Add placeholder component for labelExporting

---
+++ src/Components/LabelExporter.jsx
@@ -0,0 +1,8 @@
+// React imports
+import React from "react";
+
+const LabelExporter = () => {
+  return <div>Label exporter placeholder</div>;
+};
+
+export default LabelExporter;
--- src/static/js/labelCreationSteps.js
+++ src/static/js/labelCreationSteps.js
@@ -1,9 +1,7 @@
-// import ModelExplorer from "Components/ModelExplorer";
 import FileUploaderContainer from "Containers/FileUploaderContainer";
 import LabelTypeCreator from "Components/LabelTypeCreator";
 import LabelToolContainer from "Containers/LabelToolContainer";
-// import SplitDataset from "Components/SplitDataset";
-// import TrainModel from "Components/TrainModel";
+import LabelExporter from "Components/LabelExporter";
 
 function Step(title, id, component) {
   this.title = title;
@@ -12,21 +10,17 @@ function Step(title, id, component) {
 }
 
 export const labelCreationStepNames = [
-  // "Choose Model Type",
   "Upload Files",
   "Create Labels",
-  "Label Dataset"
-  // "Split Dataset",
-  // "Train Model"
+  "Label Dataset",
+  "Export Labels"
 ];
 
 export const labelCreationSteps = [
-  // new Step("Choose Model Type", 0, ModelExplorer),
   new Step("Upload Files", 0, FileUploaderContainer),
   new Step("Create Labels", 1, LabelTypeCreator),
-  new Step("Label Dataset", 2, LabelToolContainer)
-  // new Step("Split Dataset", 4, SplitDataset),
-  // new Step("Train Model", 5, TrainModel)
+  new Step("Label Dataset", 2, LabelToolContainer),
+  new Step("Export Labels", 3, LabelExporter)
 ];
 
 export const numberOfLabelCreationSteps = labelCreationSteps.length;
