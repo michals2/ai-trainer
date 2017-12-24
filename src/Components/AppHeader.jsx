1c6cf581cb3f9de21f039611a01e3f21cac0ace6
Luke <luke.michals@gmail.com>
2017-12-23 19:25:52 -0800

    Simplify UI by removing about information to the landing page

--- src/Components/AppHeader.jsx
+++ src/Components/AppHeader.jsx
@@ -9,20 +9,14 @@ const AppHeader = props => {
       <Menu
         theme="dark"
         mode="horizontal"
-        defaultSelectedKeys={["1"]}
+        defaultSelectedKeys={["2"]}
         style={{ height: 60 }}
       >
         <Menu.Item key="1">
           <Link to="/">TensorFlex</Link>
         </Menu.Item>
         <Menu.Item key="2">
-          <Link to={`/MyImages`}>My Images</Link>
-        </Menu.Item>
-        <Menu.Item key="3">
           <Link to={`/ImageLabeler/${modelCreatorStep}`}>Label Images</Link>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Link to="/About">About</Link>
         </Menu.Item>
       </Menu>
     </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+const sectionStyle = { padding: 20 };
+
 const Home = () =>
   <div>
-    TensorFlex is a tool to make labeling images for machine learning purposes
-    dead simple. Check out The label images tab to play around with the tool.
+    <div style={sectionStyle}>
+      TensorFlex is a tool to make labeling images for machine learning purposes
+      dead simple. Check out the label images tab to play around with the tool.
+    </div>
+    <div style={sectionStyle}>
+      I built TensorFlex because I didn't see many image labeling tools that had
+      all of the features that I wanted.
+    </div>
+    <div style={sectionStyle}>
+      I see this tool expanding in features in the future and possibly growing
+      to include model training in the browser.
+    </div>
+    <div style={sectionStyle}>
+      If you're interested in contributing to the project, check out the
+      <a href="https://github.com/michals2/tensorflex"> github</a> or reach out
+      to me directly at
+      <a href="mailto:luke.michals@gmail.com"> luke.michals@gmail.com</a>
+    </div>
   </div>;
 
 export default Home;
